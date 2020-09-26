0ace0d0db6bf52c9c89454463da3e19ce909e509
jarhill0 <git@reeshill.net>
2020-09-26 14:26:31 -0700

    Clean up no matter what in demo

--- src/demo_text_interface.py
+++ src/demo_text_interface.py
@@ -10,6 +10,9 @@ def main():
             driver[int(input('Enter relay to disable: '))] = False
     except KeyboardInterrupt:
         driver.stop()
+    except Exception:
+        driver.stop()
+        raise
 
 
 if __name__ == '__main__':
