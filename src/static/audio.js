4619f2b4f816f9161ed08f74ab30996632d50a5b
jarhill0 <git@reeshill.net>
2020-09-27 12:38:16 -0700

    Add "minimum mouth open time" option

--- src/static/audio.js
+++ src/static/audio.js
@@ -1,6 +1,7 @@
 const thresholdElement = document.getElementById('threshold');
 const mouthButton = document.getElementById('mouth-button')
 const increasingElement = document.getElementById('increasing-box')
+const openTimeElement = document.getElementById('min-open-time')
 
 let audioStarted = false;
 let requireIncreasing = false;
@@ -8,6 +9,7 @@ let recorder = null;
 let peakMeter = null;
 let meterNodeRaw = null;
 let threshold = -19;
+let minOpenTime = 0;
 
 
 function startAudio() {
@@ -25,6 +27,8 @@ function startAudio() {
 
     threshold = parseInt(thresholdElement.value, 10);
     thresholdElement.disabled = true;
+    minOpenTime = parseInt(openTimeElement.value, 10);
+    openTimeElement.disabled = true;
     requireIncreasing = increasingElement.checked;
     increasingElement.disabled = true;
     recorder.startRecording();
@@ -35,18 +39,26 @@ function stopAudio() {
     audioStarted = false;
     thresholdElement.disabled = false;
     increasingElement.disabled = false;
+    openTimeElement.disabled = false;
     document.getElementById('recording-meter').innerText = '';
     recorder.stopRecording();
 }
 
 let last = 0;
+let lastRisingEdge = 0;
 function takeAmplitude(amplitude) {
     if (amplitude > threshold && (!requireIncreasing || amplitude > last)) {
         last = amplitude
+        if (!mouthOpen)
+            lastRisingEdge = Date.now();
         mouthButton.style.backgroundColor = "red";
         return openMouth()
     }
     last = amplitude
+
+    if (Date.now() - lastRisingEdge < minOpenTime)
+        return;
+
     mouthButton.style.backgroundColor = "";
     return closeMouth();
 }
--- src/static/mouth.js
+++ src/static/mouth.js
@@ -1,7 +1,11 @@
+let mouthOpen = false;
+
 function openMouth() {
     webSocket.send('{"m":1}');
+    mouthOpen = true;
 }
 
 function closeMouth() {
     webSocket.send('{"m":0}');
+    mouthOpen = false;
 }
