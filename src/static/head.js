57d55747851dd3b29daed884e526ae82317a765b
jarhill0 <git@reeshill.net>
2020-10-21 19:58:59 -0700

    Add head support to frontend

---
+++ src/static/head.js
@@ -0,0 +1,20 @@
+const UP_ARROW = document.getElementById('up-arrow');
+const DOWN_ARROW = document.getElementById('down-arrow');
+const LEFT_ARROW = document.getElementById('left-arrow');
+const RIGHT_ARROW = document.getElementById('right-arrow');
+const ARROWS = {u: UP_ARROW, d: DOWN_ARROW, l: LEFT_ARROW, r: RIGHT_ARROW};
+
+function moveHead(where) {
+    webSocket.send(`{"h":"${where}"}`);
+}
+
+registerListener(
+    data => {
+        const headPlace = data['h'];
+        if (headPlace !== undefined) {
+            for (const [key, element] of Object.entries(ARROWS)) {
+                element.style.backgroundColor = key === headPlace ? 'red' : '';
+            }
+        }
+    }
+);
